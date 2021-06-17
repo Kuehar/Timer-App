dd440b8c4561653c239687ce43c5b28d3add8a49
kuehar <green95251@gmail.com>
2021-06-17 23:59:48 +0900

    added timer code

---
+++ Timer-App/SettingView.swift
@@ -0,0 +1,28 @@
+//
+//  SettingView.swift
+//  Timer-App
+//
+//  Created by kuehar on 2021/06/17.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text("タイマー画面")
+            }.navigationBarItems(trailing: NavigationLink(destination:SettingView()){
+                Text("秒数設定")
+                }
+                )
+            }
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
