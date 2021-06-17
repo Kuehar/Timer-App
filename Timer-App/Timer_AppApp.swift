2a2ac96dd7411af8ebf318d3dfce3dd8dcf93db5
kuehar <green95251@gmail.com>
2021-06-17 23:54:06 +0900

    Initial Commit

---
+++ Timer-App/Timer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_AppApp.swift
+//  Timer-App
+//
+//  Created by kuehar on 2021/06/17.
+//
+
+import SwiftUI
+
+@main
+struct Timer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
