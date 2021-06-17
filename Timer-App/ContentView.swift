90ddbec1035dc251804c2c58033e4190ea3e42bd
kuehar <green95251@gmail.com>
2021-06-18 00:24:22 +0900

    place button to initialize timer

--- Timer-App/ContentView.swift
+++ Timer-App/ContentView.swift
@@ -10,9 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView{
-            VStack{
-                Text("タイマー画面")
+            // 背景画像の設定
+            ZStack{
+                Image("backgroundTimer")
+                    .resizable()
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                
+                
+                VStack(spacing:30.0){
+                    Text("残り10秒")
+                        .font(.largeTitle)
+                    
+                    HStack{
+                        Button(action: {
+                            // ボタンタップ時のアクション設定
+                        }) {
+                            Text("スタート")
+                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(Color.white)
+                                .frame(width:140,height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
             }
+            
             
             .navigationBarItems(trailing: NavigationLink(destination:SettingView()){
                 Text("秒数設定")
